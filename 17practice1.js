1b49a5e024ee6bec35e5c06d705ef2cf279f68a7
Durga2408 <144308242+Durga2408@users.noreply.github.com>
2023-10-17 17:04:17 +0530

    Add files via upload

---
+++ 17practice1.js
@@ -0,0 +1,80 @@
+//int
+let a=50;
+console.log("number:"+typeof a);
+//float
+let b=19999.356387;
+console.log("float:"+typeof b);
+//char
+var c='&';
+console.log("char:"+c);
+//double 
+const d=7032481609;
+console.log("double:"+d)
+//long
+let long_datatype=12263456925824255;
+console.log("long:"+long_datatype);
+//boolean
+let boolean_datatype=true;
+console.log("boolean_datatype:"+typeof boolean_datatype);
+//string
+let string_datatype="Durga";
+console.log("string_datatype:"+typeof string_datatype);
+
+
+
+//operators
+//arthematic operaters
+//addtion
+let raju=3;
+let kamal=5;
+console.log(raju+kamal);
+//substraction
+let e=4;
+let f=6;
+console.log(e-f);
+//mutiple
+let g=7;
+let h=9;
+console.log(g*h);
+//divison
+let i=9;
+let j=2;
+console.log(i/j);
+//modules
+let k=89;
+let l=7;
+console.log(k%l);
+//conditional statement
+let x=90;
+let y=80;
+
+if (x==y)
+{
+    console.log("True");
+}
+else{
+    console.log("False");
+}
+
+
+
+
+let w=true;
+let z=false;
+if (w>z)
+{
+    console.log("To protect the phone");
+}
+else{
+    console.log("tttt");
+}
+//and or not
+let r=89;
+let s=638;
+if (r>s || r==s)
+{
+    console.log("lazy");
+}
+else{
+    console.log("worst");
+}
